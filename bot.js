43d89120be8766621695a63cf76201cc631d2fb2
michalll000 <40803091+michalll000@users.noreply.github.com>
2018-08-30 08:51:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,5 @@
-const botconfig = require("./botconfig.json");
-const tokenfile = require("./token.json");
-const Discord = require("discord.js");
-const fs = require("fs");
-const api = `dc6zaTOxFJmzC`;
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
 
 const bot = new Discord.Client({disableEveryone: true});
@@ -42,54 +39,11 @@ bot.on("guildMemberRemove", (member, guild) => {
 
 
 
-bot.on("message", async message => {
-
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(); //def 1
-  let sayMessage = args.join(" ");
-
-
-  //>>> KOMENDY <<<
-   if(args) {
-     if(message.author.bot)
-       console.log(`[SERWER]: ${message.member.guild} | #${message.channel.name} ([BOT] ${message.author.username}):`, sayMessage);
-     else if (message.author.bot === false)
-       console.log(`[SERWER]: ${message.member.guild} | #${message.channel.name} (${message.author.username}):`, sayMessage);
-   }
-
- });
-
-
-
-
-
- // [SYSTEM] Blokowanie używania na priv i przez BOTy
- bot.on("message", async message => {
-   if(message.author.bot) return;
-   if(message.channel.type === "dm") return;
-
-
-
- // [SYSTEM] Wysyłanie wiadomości
-   let prefix = botconfig.prefix;
-   let messageArray = message.content.split(" ");
-   let cmd = messageArray[0];
-   let args = messageArray.slice(1); //def 1
-
-
-
-
-
-
-
-
-
- });
-
-
-
+bot.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
  // [SYSTEM] Logowanie BOTa
  bot.login(process.env.BOT_TOKEN);
