ed97c8142c805cc238065850bd266865d1b5ae61
michalll000 <40803091+michalll000@users.noreply.github.com>
2018-08-30 13:51:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const bot = new Discord.Client();
 
 
 client.on('ready', () => {
@@ -9,10 +10,69 @@ client.on('ready', () => {
         console.log("[STATUS] Szyld załadowany!");
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+//>>> KOMENDY <<<
+  if(args) {
+    if(message.author.bot)
+      console.log(`[SERWER]: ${message.member.guild} | #${message.channel.name} ([BOT] ${message.author.username}):`, sayMessage);
+    else if (message.author.bot === false)
+      console.log(`[SERWER]: ${message.member.guild} | #${message.channel.name} (${message.author.username}):`, sayMessage);
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// [SYSTEM] Blokowanie używania na priv i przez BOTy
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+
+
+// [SYSTEM] Wysyłanie wiadomości
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1); //def 1
+
+
+
+
+// >>> KOMENDY <<<
+  //if(cmd) {
+    //console.log(`[SERWER]: ${message.member.guild} (${message.author.username}):`, cmd);
+  //};
+
+
+
+
+  // PING
+  if(cmd === `${prefix}ping`) {
+     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+     const m = await message.channel.send("Ping?");
+     m.edit(`Pong! Czas od komendy do odpowiedzi: ${m.createdTimestamp - message.createdTimestamp}ms. Pomiar TC BOT to ${Math.round(bot.ping)}ms`);
+   }
 });
 
 // THIS  MUST  BE  THIS  WAY
