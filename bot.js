d617dd67301843ab8892b27e31d1369e991252b4
michalll000 <40803091+michalll000@users.noreply.github.com>
2018-08-30 08:58:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    client.user.setActivity(`.cmds | Na ${bot.guilds.size} serwerach!`, {type: "WATCHING"});
 });
 
 client.on('message', message => {
