142aff4539ec45bef4dd840022b2e2371fdc4981
michalll000 <40803091+michalll000@users.noreply.github.com>
2018-08-30 09:17:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
     client.user.setStatus('dnd', 'TEST');
+    client.user.setGame('GAME HERE');
 });
 
 client.on('message', message => {
