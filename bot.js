32f151c454c2f841beb1ff98fcb41cd6749a31db
michalll000 <40803091+michalll000@users.noreply.github.com>
2018-08-30 09:23:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,6 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setStatus('idle')
-        .then(console.log)
-        .catch(console.error);
 });
 
 client.on('message', message => {
