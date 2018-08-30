70a964f9246967721f3ca4f18bd7b12811f874cd
michalll000 <40803091+michalll000@users.noreply.github.com>
2018-08-30 10:21:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require('fs');
-bot.commands = new Discord.Collection();
 
-fs.readdir("./commands/", (err, files) => {
-    
-    if(err) console.log(err);
-    
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if jsfile.length <= 0){
-        console.log("Nie znaleziono komendy");
-        return;
-    }
-    
-    jsfile.forEach((f, i) =>{
-        let props = require(`./commands/${f}`);
-        console.log(${f} załadowany!);
-        bot.commands.set(props.help.name, props);
-    });
-    
-});
 
 client.on('ready', () => {
     client.user.setStatus('idle')
+    client.user.setGame('nope')
 });
 
 client.on('message', message => {
